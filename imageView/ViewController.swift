17066de2cdb784b6fbcd4cd909b3d036769b59d5
AndrewL123456 <andrewlujan763@gmail.com>
2022-01-18 14:10:24 -0700

    finished product

--- imageView/ViewController.swift
+++ imageView/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let myimage = UIImage(named: "MLBaseball")
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView.image = myimage
     }
-
+    
+    
+    
+    @IBAction func changeImage(_ sender: Any) {
+        let myimage = UIImage(named: "baseballSide")
+        imageView.image = myimage
+    }
+    
+    
+    
+    
+    
 
 }
 
